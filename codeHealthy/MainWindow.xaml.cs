8dfd71a371c54b32e347e21c0d5ed49634e472b4
Alfred Wu <awu123@gmail.com>
2019-07-22 12:48:21 -0700

    Initial

--- codeHealthy/MainWindow.xaml.cs
+++ codeHealthy/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,24 @@ namespace codeHealthy
         public MainWindow()
         {
             InitializeComponent();
+
+            //DateTime currentTime = DateTime.Now;
+
+            //MessageBox.Show(currentTime.Hour.ToString());
+            //MessageBox.Show(currentTime.Minute.ToString());
+
+            int startHour = 9;
+            int startMinute = 0;
+
+            while (true)
+            {
+                Thread.Sleep(3000);
+
+                DateTime currentTime = DateTime.Now;
+                MessageBox.Show(currentTime.Second.ToString());
+
+            }
+            
         }
     }
 }
