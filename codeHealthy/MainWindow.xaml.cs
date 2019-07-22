ebfdccd90092f7f34897b78f02f470dbb99b2590
Alfred Wu <awu123@gmail.com>
2019-07-22 14:59:02 -0700

    Correct code path.

--- codeHealthy/MainWindow.xaml.cs
+++ codeHealthy/MainWindow.xaml.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using WPFCustomMessageBox;
 
 namespace codeHealthy
 {
@@ -21,27 +22,77 @@ namespace codeHealthy
     /// </summary>
     public partial class MainWindow : Window
     {
+        int score = 0;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //DateTime currentTime = DateTime.Now;
 
-            //MessageBox.Show(currentTime.Hour.ToString());
-            //MessageBox.Show(currentTime.Minute.ToString());
 
-            int startHour = 9;
-            int startMinute = 0;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += timer_Tick;
+            timer.Start();
 
-            while (true)
-            {
-                Thread.Sleep(3000);
 
-                DateTime currentTime = DateTime.Now;
-                MessageBox.Show(currentTime.Second.ToString());
-
-            }
-            
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Display.Text = "Score: " + score.ToString();
+
+            //DateTime.Now.Minute=
+
+            goodMorning();
+
+        }
+
+        void goodMorning()
+        {
+            MessageBox.Show("Good Morning", "CodeHealthy", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+            MessageBoxResult result1 = CustomMessageBox.ShowYesNo("Please wipe off your screen, keyboard and mouse.", "CodeHealthy", "Cool! I just did that.", "I don't care.");
+            switch (result1)
+            {
+                case MessageBoxResult.Yes:
+                    //MessageBox.Show("Yep", "CodeHealthy");
+                    score += 1;
+                    break;
+                case MessageBoxResult.No:
+                    //MessageBox.Show("I don't care", "CodeHealthy");
+                    score -= 1;
+                    break;
+            }
+
+            MessageBoxResult result2 = MessageBox.Show("Remove all items from your back pocket, put pillow behind your back", "CodeHealthy", MessageBoxButton.YesNo);
+            switch (result2)
+            {
+                case MessageBoxResult.Yes:
+                    MessageBox.Show("Cool! Just did that.", "CodeHealthy");
+                    score += 1;
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("I don't care", "CodeHealthy");
+                    score -= 1;
+                    break;
+            }
+
+            MessageBoxResult result3 = MessageBox.Show("Did you say hi to all your team members?", "CodeHealthy", MessageBoxButton.YesNo);
+            switch (result3)
+            {
+                case MessageBoxResult.Yes:
+                    MessageBox.Show("Yep, just did that.", "CodeHealthy");
+                    score += 1;
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("I don't care", "CodeHealthy");
+                    MessageBox.Show("Saying Hi to your team mates help you build stronger relationship.", "CodeHealthy", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                    score -= 1;
+                    break;
+            }
+
+        }
+
     }
 }
